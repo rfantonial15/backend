825828311107c60f4efe7df096bcceca71e94483
Christian Macatangay <cmacatangay3433@gmail.com>
2024-10-16 22:30:11 +0800

    Update SendAlert

--- aidabackend/alert/serializers.py
+++ aidabackend/alert/serializers.py
@@ -1,11 +1,10 @@
+from datetime import datetime
 from rest_framework import serializers
 from .models import Alert
-from datetime import datetime
 
 class AlertSerializer(serializers.ModelSerializer):
-    time = serializers.DateTimeField(format='%I:%M %p', read_only=True)
-    timeDate = serializers.DateTimeField(format='%B %d, %Y - %I:%M:%S %p', default=datetime.now)
-
+    time = serializers.DateTimeField(format='%B %d, %Y - %I:%M:%S %p', default=datetime.now)
+    
     class Meta:
         model = Alert
-        fields = ['id', 'subject', 'message', 'image', 'files', 'links', 'time', 'timeDate']
+        fields = ['id', 'subject', 'message', 'image', 'files', 'links', 'time']  # Include the time field, but it's read-only
